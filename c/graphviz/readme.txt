3c188a8e2c195d928cc8c6fcde526e84af573874
shuwt <418674879@qq.com>
2020-08-10 10:46:59 +0800

    upload graphviz

---
+++ c/graphviz/readme.txt
@@ -0,0 +1,4 @@
+step1: apt install graphviz
+step2: pip install gprof2dot
+step3: apt install xdot
+step4: make
---
+++ c/graphviz/test.cpp
@@ -0,0 +1,58 @@
+#include <stdio.h>
+/*
+This example use for learning gprof
+*/
+
+#define MAX_NUM 10000000
+
+int g_count = 10;
+
+void fun_1()
+{
+	int i = 0;	
+	int g = 0;	
+	for(;i < MAX_NUM ; ++ i)
+	{
+		g += i*i;
+	}
+}
+
+
+void fun_2()
+{
+	int i = 0;	
+	int g = 0;	
+	for(;i < MAX_NUM * 4 ; ++ i)
+	{
+		g += i*i;
+	}
+}
+
+void fun_3()
+{
+	if(g_count < 0)
+	{
+		return ;
+	}
+
+	g_count --;
+
+	fun_1();
+	fun_2();
+	fun_3();
+}
+
+void (*fun)();
+
+int main()
+{
+	printf("hello world\n");
+
+	void (* fun)();
+
+	fun = fun_3;
+
+	fun();
+
+	return 0;
+}
